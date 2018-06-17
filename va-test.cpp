5eda4bea062891018e76f6ccb535d50d4b23e946
Henryk Paluch <henryk.paluch@pickering.cz>
2018-06-17 10:03:30 +0200

    Removed excessive ws

--- va-test.cpp
+++ va-test.cpp
@@ -6,18 +6,17 @@
 // uncomment to buggy build (will crash when run)
 //#define MY_BUG_VA_ARGS
 
-
 void writeShort(unsigned short v){
 	printf("Short value=%hu\n",v);
-
 }
 
-void arg_test(int n,...){
+void arg_test(int n, ...){
 
 	va_list ap;
 
+	// n is ignored by us, but va_start() requires name of last
+	// argument before ...
 	va_start(ap,n);
-
 	printf("i1=%d\n",va_arg(ap,int));
 
 #ifdef MY_BUG_VA_ARGS
@@ -28,17 +27,12 @@ void arg_test(int n,...){
 	writeShort((unsigned short)va_arg(ap,unsigned int));
 #endif
 
-
 	printf("i2=%d\n",va_arg(ap,int));
-
-
 	va_end(ap);
-
 }
 
 
 int main(int argc, char **argv) {
-
 	int i1=1234567890,i2=987654321;
 	unsigned short s1=32123;
 
